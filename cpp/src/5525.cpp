89aabcdb42cc8d1f2f419b993bd9b3c8cc277af5
looooooq <106612302+parksinjo@users.noreply.github.com>
2026-01-25 22:26:01 +0900

    solve: BOJ 5525 (C++)

---
+++ cpp/src/5525.cpp
@@ -0,0 +1,60 @@
+/*
+[BOJ] 5525 - IOIOI
+https://www.acmicpc.net/problem/5525
+- Algorithm: string matching
+- Data Structure: string
+- Time: O(N)
+- Space: O(N)
+- Trick:
+  *  문자열 매칭 알고리즘을 사용하여 패턴 "IOI"가 몇 번 연속으로 나타나는지 계산
+*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+/* 1차 코드 
+int main(){
+    int n;
+    cin >> n;
+    string ioi = "IOI";
+    for(int i=1; i<n; ++i){
+        ioi+="OI";
+    }
+    cin >> n;
+    string s;
+    cin >> s;
+    int count = 0;
+    size_t start = 0;
+    while(1){
+        size_t pos = s.find(ioi, start);
+        if(pos == string::npos) break;
+        count++;
+        start = pos + 1;
+    }
+    cout << count << "\n";
+}
+*/
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    string s;
+    cin >> s;
+    int count = 0;
+   for(int i=0; i<m; ++i){
+        int c = 0;
+        if(s[i] == 'O') continue;
+        else {
+            int index = i;
+            while(s[index+1] == 'O' && s[index+2] == 'I'){
+                c++;
+                if(c == n){
+                    count++;
+                    break;
+                }
+                index += 2;
+            }
+        }
+    }
+cout << count << "\n";
+}
